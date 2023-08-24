51665dd66614b77e44e435987d769dfb934efa3f
amalshweiki <amalshweiki88@gmail.com>
2023-08-24 13:29:08 +0300

    data type

---
+++ index.js
@@ -0,0 +1,31 @@
+let myString = "My Name is Amal";
+let MyAge = 35;
+let MyB = true;
+let MyUND;
+let myNull = null;
+let mysymbol = Symbol("id");
+
+console.log(myString);
+console.log(MyAge);
+console.log(MyB);
+console.log(MyUND);
+console.log(myNull);
+console.log(mysymbol);
+/////type of
+console.log("type of variables");
+console.log(typeof myString);
+console.log(typeof MyAge);
+console.log(typeof MyB);
+console.log(typeof MyUND);
+console.log(typeof myNull);
+console.log(typeof mysymbol);
+///////ex2
+let a = "5";
+let b = 3;
+let c = a * b;
+let d = a + b;
+let e = false;
+let f = e + a;
+console.log(c); // What will be the output?
+console.log(d); // What will be the output?
+console.log(f); // What will be the output?
